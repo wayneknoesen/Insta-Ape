0a931da433061c3eb56cfd2ed96fbc845415cd67
Wayne Knoesen <wayne.knoesen@apegroup.com>
2014-05-25 12:20:15 +0200

    Initial Commit

---
+++ Photo Bombers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Photo Bombers
+//
+//  Created by Wayne Knoesen on 25/05/14.
+//  Copyright (c) 2014 Wayne Knoesen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Photo Bombers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photo Bombers
+//
+//  Created by Wayne Knoesen on 25/05/14.
+//  Copyright (c) 2014 Wayne Knoesen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
